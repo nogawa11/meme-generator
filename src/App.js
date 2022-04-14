13cf567c5f0817e54b98e83e278d4ead0d45c513
Nicole Ogawa <nogawa11@gmail.com>
2022-04-14 22:03:38 +0900

    updated form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import Navbar from './components/Navbar';
 import Meme from './components/Meme';
-import Box from './components/Box';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Meme />
-      <div className="boxes">
-        <Box />
-      </div>
     </div>
   );
 }
