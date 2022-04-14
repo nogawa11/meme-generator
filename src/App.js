00d3816f3879f347a2864742557d9b18639577ee
Nicole Ogawa <nogawa11@gmail.com>
2022-04-14 22:02:36 +0900

    updated form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from './components/Navbar';
 import Meme from './components/Meme';
+import Box from './components/Box';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Meme />
+      <div className="boxes">
+        <Box />
+      </div>
     </div>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,23 +2,45 @@ import memesData from "./memesData.js";
 import React from 'react';
 
 export default function Meme() {
-  const [url, setMeme] = React.useState("")
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
   function getMemeImage() {
     const memesArray = memesData.data.memes
     const randomNum = Math.floor(Math.random() * memesArray.length)
-    setMeme(memesArray[randomNum].url)
+    const url = memesArray[randomNum].url
+    setMeme(prevMeme => ({...prevMeme, randomImage: url}))
+  }
+
+  function handleChange(event) {
+    setMeme(prevState => ({...prevState, [event.target.name]: event.target.value}))
+    console.log(meme)
   }
 
   return (
     <main>
       <div className="form">
         <div className="form--input">
-          <input type="text" placeholder="Top text"/>
-          <input type="text"placeholder="Bottom text"/>
+          <input type="text"
+            placeholder="Top text"
+            onChange={handleChange}
+            name="topText"
+            value={meme.topText}
+          />
+          <input type="text"
+            placeholder="Bottom text"
+            onChange={handleChange}
+            name="bottomText"
+            value={meme.bottomText}
+          />
         </div>
         <button onClick={getMemeImage} className="form--btn" type="submit">Get a new meme image 🖼</button>
-        <img className="meme--image" src={url ? url : 'https://i.imgflip.com/30b1gx.jpg'} alt="meme" />
+        <img className="meme--image" src={meme.randomImage} alt="meme" />
       </div>
     </main>
   )
