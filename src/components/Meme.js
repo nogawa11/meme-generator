427a31e951825bee99b6d87d13c24f969a91b0e3
Nicole Ogawa <nogawa11@gmail.com>
2022-04-14 20:14:43 +0900

    added image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,17 +1,25 @@
+import memesData from "./memesData.js";
+import React from 'react';
+
 export default function Meme() {
-  const handleClick = () => {
-    console.log("click")
+  const [url, setMeme] = React.useState("")
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNum = Math.floor(Math.random() * memesArray.length)
+    setMeme(memesArray[randomNum].url)
   }
 
   return (
     <main>
-      <form>
+      <div className="form">
         <div className="form--input">
-          <input type="text" placeholder="Shut up"/>
-          <input type="text"placeholder="Take my money"/>
+          <input type="text" placeholder="Top text"/>
+          <input type="text"placeholder="Bottom text"/>
         </div>
-        <button onclick={handleClick} className="form--btn" type="submit">Get a new meme image 🖼</button>
-      </form>
+        <button onClick={getMemeImage} className="form--btn" type="submit">Get a new meme image 🖼</button>
+        <img className="meme--image" src={url ? url : 'https://i.imgflip.com/30b1gx.jpg'} alt="meme" />
+      </div>
     </main>
   )
 }
